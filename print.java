5ddbff8c204d012979656862f810425349bd9125
Randeep Rana <157797261+Rana-Randeep@users.noreply.github.com>
2024-09-02 10:55:48 +0530

    Add files via upload

---
+++ print.java
@@ -0,0 +1,15 @@
+class print{
+    public static void main(String args[]){
+        System.out.println("hello world");
+         System.out.print("hello java \n");
+
+        System.out.print("hello buddy ");
+          System.out.print("hello guys \n");
+
+        System.out.println("*");
+         System.out.println("**");
+
+        System.out.println("***");
+         System.out.println("*****");
+    }
+}
---
+++ printhello.java
@@ -0,0 +1,7 @@
+class printhello{
+    public static void main(String args[]){
+        System.out.println("hello world in java");
+         System.out.print("hello world in java ");
+          System.out.print("hello world in java");
+    }
+}
