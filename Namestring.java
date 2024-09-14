37846e6683b21901dcfe2864f0b26da4ecb403b4
Randeep Rana <ranarandeep521@gmail.com>
2024-09-14 21:58:30 +0530

    Add files via upload

---
+++ Namestring.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Namestring{
+    public static void main(String args[]){
+        Scanner we=new Scanner(System.in);
+        String name="Randeep Rana.";
+        System.out.println(name);
+        String sentence="MY NAME IS RAAJ SINGH.";
+        System.out.println(sentence);
+
+        //TAKING STRING INPUT
+        System.out.println("ENTER YOUR NAME");
+        String naam=we.nextLine();
+        System.out.println(naam);
+       
+        System.out.println("ENTER YOUR FULL NAME");
+        String r=we.next();
+        System.out.println(r);
+    }
+}
