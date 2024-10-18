1907843dc750bcd656e7cebafc693d30f1541b94
Randeep Rana <ranarandeep521@gmail.com>
2024-10-18 08:47:09 +0530

    Add files via upload

---
+++ StaticNc2.java
@@ -0,0 +1,12 @@
+// STATIC NESTED CLASS - 2
+class StaticNc2{
+    static class Inner{
+      public static void main(String args[]){
+        System.out.println("STATIC NESTED CLASS MAIN METHOD");
+      }
+    }
+    public static void main(String args[]){
+        System.out.println("OUTER NESTED CLASS MAIN METHOD ");
+    }
+}
+//WRITE CODE TO PRINT INNER CLASS METHODS
